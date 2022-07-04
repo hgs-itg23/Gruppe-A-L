359c9fbb7db18b6a892d84cccfd02935e627751a
Alemani0 <Alex.Urkaev@yahoo.com>
2022-07-04 20:22:01 +0200

    paar bilder

--- Projekt-Choice/src/hgs/itg23/Abfrage.java
+++ Projekt-Choice/src/hgs/itg23/Abfrage.java
@@ -8,74 +8,135 @@ import javax.swing.*;
 public class Abfrage implements ActionListener {
 
 	int i = 0;
-	ImageIcon B1 = new ImageIcon("pac.png");
-	ImageIcon B2 = new ImageIcon("wen.png");
-	ImageIcon B3 = new ImageIcon("wa.png");
-	ImageIcon B4 = new ImageIcon("cc.jpg");
-	ImageIcon B5 = new ImageIcon("ll.jpg");
-	ImageIcon B6 = new ImageIcon("nn.jpg");
-	ImageIcon B7 = new ImageIcon("pac.png");
+	ImageIcon B1 = new ImageIcon("valo.jpg");
+	ImageIcon B2 = new ImageIcon("elden.jpg");
+	ImageIcon B3 = new ImageIcon("fn.jpg");
+	ImageIcon B4 = new ImageIcon("mc.jpg");
+	ImageIcon B5 = new ImageIcon("mario.png");
+	ImageIcon B6 = new ImageIcon("Download.jpg");
+	ImageIcon B7 = new ImageIcon("bo2.jpg");
+	ImageIcon B8 = new ImageIcon("battle.jpg");
+	ImageIcon B9 = new ImageIcon("botw.jpg");
+	ImageIcon B10 = new ImageIcon("fn.jpg");
+	ImageIcon B12 = new ImageIcon("cc.jpg");
+	ImageIcon B13 = new ImageIcon("ll.jpg");
+	ImageIcon B14 = new ImageIcon("nn.jpg");
+	ImageIcon B15 = new ImageIcon("pac.png");
+	ImageIcon B16 = new ImageIcon("valo.jpg");
+	ImageIcon B17 = new ImageIcon("elden.jpg");
+	ImageIcon B18 = new ImageIcon("fn.jpg");
+	ImageIcon B19 = new ImageIcon("cc.jpg");
+	ImageIcon B20 = new ImageIcon("ll.jpg");
+	ImageIcon B21 = new ImageIcon("nn.jpg");
+	ImageIcon B22 = new ImageIcon("pac.png");
+	ImageIcon B23 = new ImageIcon("nn.jpg");
+	ImageIcon B24 = new ImageIcon("pac.png");
 	
 	
-	String[] fragen = {			"Beliebtestes Spielt derzeit?",
-								"Meist verkauftes Spiel allerzeiten?",
-							    "Belienteste Spiel-Genre?",
-							   
-							    
-							    "Wie viel Menschen leben in Deutschland?",
-							    "Welches ist das kleinste Land der Welt?",
-							    "Welches Land hat die nördlichste Hauptstadt? ",
-							    
-							    
-							    "Wie viel Bundesländer hat Deutschland?",
-							    "Was ist die größte Stadt der Welt?",
-							    "Welchen Wert hat Pi(π)?"
-							    
-				      };
+	String[] fragen = {            "Beliebtestes Spielt derzeit?",
+            "Meist verkauftes Spiel allerzeiten?",
+            "Belienteste Spiel-Genre?",
+
+
+            "Wie viel Menschen leben in Deutschland?",
+            "Welches ist das kleinste Land der Welt?",
+            "Welches Land hat die nördlichste Hauptstadt? ",
+
+
+            "Wie viel Bundesländer hat Deutschland?",
+            "Was ist die größte Stadt der Welt?",
+            "Welchen Wert hat Pi(π)?",
+
+
+            "Was beliebteste Netflix-Serie?",
+            "Was sind die beliebtestem Netflix-Generes?",
+            "Was ist der Beliebtester Netflix-Film?",
+
+
+            "Teurstes Auto der Welt?",
+            "Beliebteste Automarke der Welt?",
+            "Erfolgreicheste Automarke der Welt?",
+
+            "Was ist das beliebteste Haustier der Welt?",
+            "Was ist das freundlichste Tier der Welt?",
+            "Was ist das gefährlichste Tier der Welt?",
+
+  };
+	
 	
 	String[][] moeglichkeiten = {
-									{"Valorant","Elden Ring","Fortnite"},
-									{"Minecraft","Super Mario","Tetris"},
-									{"Actionspiele","Battle-Royal","Open World"},
-									
-									
-									{"81 Millionen ","82 Millionen","80 Millionen"},
-									{ "Monaco","Frankreich","Vatikanstadt"},
-									{  "Island","Russland","Irland"},
-									
-									
-									{ "16","2","14"},
-									{ "Tokio","Moskau","Shanghai"},
-									{ "3,14","3,44","3,46"},
-									
-									
-	};
+            {"Valorant","Elden Ring","Fortnite"},
+            {"Minecraft","Super Mario","Tetris"},
+            {"Actionspiele","Battle-Royal","Open World"},
+
+
+            {"81 Millionen ","82 Millionen","80 Millionen"},
+            { "Monaco","Frankreich","Vatikanstadt"},
+            {  "Island","Russland","Irland"},
+
+
+            { "16","2","14"},
+            { "Tokio","Moskau","Shanghai"},
+            { "3,14","3,44","3,46"},
+
+            { "Stranger Things","Breaking bad","Blacklist"},
+            { "Action/Abenteuer","Thriller/Horror","Komödien/Familienfilme"},
+            { "6 Underground","Spenser Confidential","The Dark knight"},
+
+
+            { "Mercedez Benz 300 SlR Uhlenhaut","Bugatti la Voiture Noire","Rolly-Royse sweptail"},
+            { "Bmw","Mercedes","Lamborghini"},
+            { "Toyota","Mercedes","Ford"},
+
+
+            { "Katzen","Vögel","Hunde"},
+            { "Faultiere","Capybaras","Koalas"},
+            { "Petermännchen(Fisch)","Portugiesische Galeere(Quallen)","Barrakudas(Fisch)"},
+
+};
 	
 	String[] antworten = {
-							"2",
-							"3",
-							"1",
-							"1",
-							"3",
-							"1",
-							"1",
-							"1",
-							"1",
-							
-							
-							
-					};
+	        "2",
+	        "3",
+	        "1",
+
+	        "1",
+	        "3",
+	        "1",
+
+	        "1",
+	        "1",
+	        "1",
+
+	        "1",
+	        "1",
+	        "2",
+
+	        "1",
+	        "1",
+	        "1",
+
+
+	        "1",
+	        "2",
+	        "2",
+
+
+
+
+	};
+
 	
-	ImageIcon[][] bilder  =  {		{B1,B3,B2,B3,B3,B3},
-									{B2,B3,B1,B3,B3,B3},
-									{B3,B1,B2,B3,B3,B3}
+	ImageIcon[][] bilder  =  {		{B1,B4,B7,B3,B3,B3},
+									{B2,B5,B8,B3,B3,B3},
+									{B3,B6,B9,B3,B3,B3}
 			
 	};
 	
 	  String erraten;
 	  String antwort;
 	  int index;
-	  int falscheantwort = 0;
+	  int richtigeantwort = 0;
 	  int alle_fragen = fragen.length;
 	  int ergebnis;
 	  int sekunden = 10;
@@ -96,8 +157,7 @@ public class Abfrage implements ActionListener {
 	JLabel Antwort1_label = new JLabel();
 	JLabel Antwort2_label = new JLabel();
 	JLabel Antwort3_label = new JLabel();
-	JLabel zeit_label = new JLabel();
-	JLabel rest_zeit = new JLabel();
+	
 	JTextField richtige_antworten = new JTextField();
 	
 	JLabel pic = new JLabel();
@@ -118,14 +178,14 @@ public class Abfrage implements ActionListener {
 		
 		
 		
-		textfeld.setBounds(155, 43, 650, 50);										//Höchste priorität
+		textfeld.setBounds(155, 43, 650, 50);										//HÃ¶chste prioritÃ¤t
 		textfeld.setBackground(Color.BLUE);										// Optional
 		textfeld.setForeground(Color.WHITE);										// Optional
-		textfeld.setFont(new Font("Ink Free", Font.BOLD,30));          				// WICHTIG! Ändern
+		textfeld.setFont(new Font("Ink Free", Font.BOLD,30));          				// WICHTIG! Ã„ndern
 		textfeld.setBorder(BorderFactory.createBevelBorder(1));						// So lassen
 		textfeld.setHorizontalAlignment(textfeld.CENTER);							// Optional	
-		textfeld.setEditable(false);												// So lassen90ßßßßßßßßßßßßßßßßß
-		textfeld.setText("Test");													//	Ändern so bald fertig
+		textfeld.setEditable(false);												// So lassen90ÃŸÃŸÃŸÃŸÃŸÃŸÃŸÃŸÃŸÃŸÃŸÃŸÃŸÃŸÃŸÃŸÃŸ
+		textfeld.setText("Test");													//	Ã„ndern so bald fertig
 										
 		
 		textbereich.setBounds(155, 93, 650, 50);	
@@ -138,7 +198,7 @@ public class Abfrage implements ActionListener {
 		textbereich.setText("Test1");			
 		
 		
-		button1.setBounds(200, 200, 500, 100); 										//ändern
+		button1.setBounds(200, 200, 500, 100); 										//Ã¤ndern
 		button1.setFont(new Font("Arial Black", Font.BOLD, 20)); 
 		button1.setBackground(Color.BLUE);
 		button1.setForeground(Color.WHITE);	
@@ -147,7 +207,7 @@ public class Abfrage implements ActionListener {
 		button1.setText("1");
 		
 		
-		button2.setBounds(200, 400, 500, 100); 										//ändern
+		button2.setBounds(200, 400, 500, 100); 										//Ã¤ndern
 		button2.setFont(new Font("Arial Black", Font.BOLD, 20)); 
 		button2.setBackground(Color.BLUE);
 		button2.setForeground(Color.WHITE);	
@@ -155,7 +215,7 @@ public class Abfrage implements ActionListener {
 		button2.addActionListener(this);
 		button2.setText("2");
 		
-		button3.setBounds(200, 600, 500, 100); 										//ändern
+		button3.setBounds(200, 600, 500, 100); 										//Ã¤ndern
 		button3.setFont(new Font("Arial Black", Font.BOLD, 20)); 
 		button3.setBackground(Color.BLUE);
 		button3.setForeground(Color.WHITE);	
@@ -163,7 +223,7 @@ public class Abfrage implements ActionListener {
 		button3.addActionListener(this);
 		button3.setText("3");
 		
-		resetbutton.setBounds(1640, 10, 250, 100); 										//ändern
+		resetbutton.setBounds(1640, 10, 250, 100); 										//Ã¤ndern
 		resetbutton.setFont(new Font("Arial Black", Font.BOLD, 20)); 
 		resetbutton.setBackground(Color.BLUE);
 		resetbutton.setForeground(Color.WHITE);	
@@ -254,7 +314,7 @@ public class Abfrage implements ActionListener {
 					textfeld.setText("Kategorie Gaming");
 				}
 				if(index==3) {
-					textfeld.setText("Kategorie Länder");
+					textfeld.setText("Kategorie LÃ¤nder");
 				}
 				if(index==6) {
 					textfeld.setText("Kategorie Allgemeinwissen");
@@ -308,7 +368,7 @@ public class Abfrage implements ActionListener {
 				
 				if (antwort == antworten[index]) {
 					
-					falscheantwort++;
+					richtigeantwort++;
 					
 				}
 				
@@ -327,7 +387,7 @@ if(e.getSource()==button2) {
 				
 				if (antwort == antworten[index]) {
 					
-					falscheantwort++;
+					richtigeantwort++;
 					
 				}
 			}
@@ -343,7 +403,7 @@ if(e.getSource()==button3) {
 	
 	if (antwort == antworten[index]) {
 		
-		falscheantwort++;
+		richtigeantwort++;
 		
 	}
 }
@@ -466,14 +526,14 @@ pic.setVisible(true);
 			button2.setEnabled(false);
 			button3.setEnabled(false);
 			
-			ergebnis = (int)((falscheantwort/(double)alle_fragen)*100);
+			ergebnis = (int)((richtigeantwort/(double)alle_fragen)*100);
 			
 			textfeld.setText("Dein Ergebnis");
 			textbereich.setText("");
 			Antwort1_label.setText("");
 			Antwort2_label.setText("");
 			Antwort3_label.setText("");
-			richtige_antworten.setText("("+falscheantwort+"/"+alle_fragen+ ")");
+			richtige_antworten.setText("("+richtigeantwort+"/"+alle_fragen+ ")");
 			
 			frame.add(richtige_antworten);
 			
